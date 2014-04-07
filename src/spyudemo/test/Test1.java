829b44fbcbb9c314514696ad684d6a52be8a659c
yu <spyu2000@126.com>
2014-04-08 01:10:31 +0800

    spyudemo

---
+++ src/spyudemo/test/Test1.java
@@ -0,0 +1,22 @@
+package spyudemo.test;
+
+import com.alibaba.fastjson.JSON;
+
+
+public class Test1 {
+
+	 public static void main(String[] args){
+	    	
+	    	
+	    	User user=new User();
+	    	user.setId(1);
+	     	user.setName("name");
+	    	String test=JSON.toJSONString(user);
+	    	System.out.println(test);
+	    	
+	    	User user1=JSON.parseObject(test, User.class);
+	    	
+	    	System.out.println(user1.getName());
+	    	
+	    }
+}
